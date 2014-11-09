d5e350cdb177135fe24032d3bc8d37a3269952c4
Sukhada Kulkarni <skulkarni@hearsaycorp.com>
2014-11-09 14:36:30 -0800

    stuffzzz

--- api/public/scripts/dinner.js
+++ api/public/scripts/dinner.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
-	if (localStorage.getItem('email')) {
-		var email = localStorage.getItem('email');
-		if (localStorage.getItem('userCreated')) {
-			user = getCurrentUser();
+	var user = getCurrentUser();
+	var dinnerRequest = apiRequest('/1/classes/Dinner', {'user_id' : user.objectId}, 'GET');	
+	if (dinnerRequest.results[0]) {
+		$('#order-dinner').addClass('btn-danger');
+		$('#order-dinner').text('Dinner Ordered!');
+		var prefs = dinnerRequest.results[0].preferences;
+		for (pref in prefs) {
+			$('#'+prefs[pref]).attr('checked', true);
 		}
 	}
 	$('#order-dinner').on('click', function() {
-
+		toggleDinner(user);
 	});
 });
 
+function toggleDinner(user) {
+	if ($('#order-dinner').hasClass('btn-danger')) {
+		var dinnerRequest = apiRequest('/1/classes/Dinner', {'user_id' : user.objectId}, 'GET');
+		var removed = apiRequest('/1/classes/Dinner/' + dinnerRequest.results[0].objectId ,{}, 'DELETE');		
+		$('#order-dinner').removeClass('btn-danger');
+		$('#order-dinner').text('Order Dinner Tonight!');		
+	}
+	else {
+		var preferences = [parseInt($("input:radio[name ='preferences']:checked").attr('id'))];
+		var restrictions = parseInt($("input:checkbox[name ='restrictions']:checked").attr('id'));
+		if (restrictions) {
+			preferences.push(restrictions);
+		}
+		var dinnerRequest = apiRequest('/1/classes/Dinner', {'user_id' : user.objectId, 'preferences' : preferences} ,'POST');
+		$('#order-dinner').addClass('btn-danger');
+		$('#order-dinner').text('Dinner Ordered!');
+	}
+}
+
 function countdown() {
 	var now = new Date();
-	var hoursLeft = 15 - now.getHours();
+	var hoursLeft = 15-now.getHours();
 	var minutesleft = 59-now.getMinutes();
 	var secondsleft = 59-now.getSeconds();	
 	//format 0 prefixes
