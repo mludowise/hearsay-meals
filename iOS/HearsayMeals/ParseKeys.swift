fbe449190e06f1e7082bc85b5baee32415300e01
mludowise <mludowise@gmail.com>
2014-11-09 14:09:48 -0800

    Logging in to parse!!

---
+++ iOS/HearsayMeals/ParseKeys.swift
@@ -0,0 +1,21 @@
+//
+//  ParseKeys.swift
+//  HearsayMeals
+//
+//  Created by Mel Ludowise on 11/9/14.
+//  Copyright (c) 2014 Mel Ludowise. All rights reserved.
+//
+
+import Foundation
+
+let kUserPassword = "password"
+
+let kUserPictureKey = "picture"
+let kUserNameKey = "name"
+let kUserAdminKey = "admin"
+
+let kBeerRequestNameKey = "name"
+let kBeerRequestUserKey = "user"
+
+let kKegBeerNameKey = "beerName"
+let kKegKickedReportsKey = "kickedReports"
