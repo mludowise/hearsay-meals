3167589e53cec636159e01fb8fc737aedeb27eee
Carlo Besozzi <cbesozzi@Carlo-Besozzis-MacBook-Pro.local>
2014-12-12 11:28:32 -0800

    added more picture pulled randomly

--- api/public/scripts/xmas.js
+++ api/public/scripts/xmas.js
@@ -1,17 +1,17 @@
 var utils = {
     randomNumber: function (n) {
-       var number =  Math.floor(Math.random() * n) + 1
-       return number;
+        var number = Math.floor(Math.random() * n) + 1;
+        return number;
     }
 }
 
 var xmas = {};
 xmas.imagesBaseUrl = './images/xmas/';
-xmas.imageName = 'spikey-plant.png';
-xmas.imageTag = '<img src="' + xmas.imagesBaseUrl + xmas.imageName + '" class="xmas-image">';
+xmas.imageNames = ['','spikey-plant.png','reindeer.png', 'bells.png', 'bird.png'];
+xmas.imageTag = '<img src="' + xmas.imagesBaseUrl + xmas.imageNames[utils.randomNumber(4)] + '" class="xmas-image">';
 xmas.appendImage = function (url) {
     $('.navbar-header').prepend(xmas.imageTag);
-    console.log(utils.randomNumber(23));
+    console.log(utils.randomNumber(1));
 };
 xmas.init = function () {
     xmas.appendImage();
