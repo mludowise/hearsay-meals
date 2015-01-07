40191674e523f01ec5a01a0349154085b7914ca5
mludowise <mludowise@gmail.com>
2015-01-07 01:17:46 -0800

    Added keys for staging

--- api/public/scripts/keys.js
+++ api/public/scripts/keys.js
@@ -8,6 +8,11 @@ function getParseKeys() {
 	    	    'X-Parse-Application-Id': 'myq9zbMzdkBqqEyudRcwIR5yxnmwihlslqUvYh34',
     	    	'X-Parse-REST-API-Key': '8Zwn2jGVUZEimJ9YYJUorF305QCAg9qZaWsUVVPZ'
 			};
+		case "staging-hearsaymeals.parseapp.com":
+			return {
+	    	    'X-Parse-Application-Id': 'ryopGELyjc2gdNRLbOcFJCWu0QKorxgF7wNcvoSd',
+    	    	'X-Parse-REST-API-Key': 'ByYhD17CyRjzUdpNlyiWzH2qhT09LhhDtSP4X8ER'
+			};
 		case "mel-hearsaymeals.parseapp.com":
 			return {
 	    	    'X-Parse-Application-Id': 'F23K1fxL2OJpfZphfH0lR0Nryz7QCuI1dwIFT6kU',
@@ -21,11 +26,11 @@ function getParseKeys() {
 function getGoogleClientId() {
  	var url = window.location.hostname
 	switch (url) {
-		case "hearsaymeals.parseapp.com":
-			return '966122623899-07cog3u9lpipk8q4qo9bvmn51v7h01m5.apps.googleusercontent.com';
+		case "staging-hearsaymeals.parseapp.com":
+			return '966122623899-407mp3e3ai9291tegm1e7rnh8vemfn5p.apps.googleusercontent.com';
 		case "mel-hearsaymeals.parseapp.com":
 			return '966122623899-a09be0hictilu42qa7v1gtu7ue5kvnc2.apps.googleusercontent.com';
-		default:
-			return null;
+		default: // "hearsaymeals.parseapp.com"
+			return '966122623899-07cog3u9lpipk8q4qo9bvmn51v7h01m5.apps.googleusercontent.com';
 	}
 }
