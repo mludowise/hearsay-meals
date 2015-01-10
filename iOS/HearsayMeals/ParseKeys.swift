04fb272d3be720181499f44e2d2297881d02216c
mludowise <mludowise@gmail.com>
2015-01-10 02:40:50 -0800

    Check for iOS updates
    https://trello.com/c/47JTd2ci/10-allow-ios-app-to-check-for-updates

--- iOS/HearsayMeals/ParseKeys.swift
+++ iOS/HearsayMeals/ParseKeys.swift
@@ -32,3 +32,9 @@ let kDinnerSpecialRequestKey = "special_request"
 
 let kCreatedAtKey = "createdAt"
 let kObjectId = "objectId"
+
+let kApplicationPropertiesTableKey = "ApplicationProperties"
+let kApplicationPropertiesType = "applicationType"
+let kApplicationPropertiesId = "applicationId"
+let kApplicationPropertiesLatestVersion = "latestVersion"
+let kApplicationPropertiesDownloadUrl = "downloadUrl"
