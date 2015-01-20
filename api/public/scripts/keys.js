9aeacae867244b3337bee08897333930eebd4f2b
mludowise <mludowise@gmail.com>
2015-01-19 23:07:54 -0800

    Updated HTML responsiveness for mobile

--- api/public/scripts/keys.js
+++ api/public/scripts/keys.js
@@ -9,11 +9,11 @@ function getParseKeys() {
 	    	    'X-Parse-Application-Id': 'myq9zbMzdkBqqEyudRcwIR5yxnmwihlslqUvYh34',
     	    	'X-Parse-REST-API-Key': '8Zwn2jGVUZEimJ9YYJUorF305QCAg9qZaWsUVVPZ'
 			};
-// 		case "mel-hearsaymeals.parseapp.com":
-// 			return {
-// 	    	    'X-Parse-Application-Id': 'F23K1fxL2OJpfZphfH0lR0Nryz7QCuI1dwIFT6kU',
-//     	    	'X-Parse-REST-API-Key': 'i9cpzriov9mLY3oNKXvBZt5xq0lAct8qDBKFuYc1'
-// 			};
+		case "mel-hearsaymeals.parseapp.com":
+			return {
+	    	    'X-Parse-Application-Id': 'F23K1fxL2OJpfZphfH0lR0Nryz7QCuI1dwIFT6kU',
+    	    	'X-Parse-REST-API-Key': 'i9cpzriov9mLY3oNKXvBZt5xq0lAct8qDBKFuYc1'
+			};
 	}
 	return null;
 }
@@ -24,8 +24,8 @@ function getGoogleClientId() {
 		case "hearsaymeals.parseapp.com":			// Production
 		case "staging-hearsaymeals.parseapp.com":	// Staging
 			return '966122623899-07cog3u9lpipk8q4qo9bvmn51v7h01m5.apps.googleusercontent.com';
-// 		case "mel-hearsaymeals.parseapp.com":
-// 			return '966122623899-a09be0hictilu42qa7v1gtu7ue5kvnc2.apps.googleusercontent.com';
+		case "mel-hearsaymeals.parseapp.com":
+			return '966122623899-a09be0hictilu42qa7v1gtu7ue5kvnc2.apps.googleusercontent.com';
 	}
 	return null;
 }
