82ad90815c27720492b1d8271b8309e9ee1a006f
mludowise <mludowise@gmail.com>
2015-01-31 04:47:56 -0800

    Feature parity with dinner

--- api/cloud/main.js
+++ api/cloud/main.js
@@ -1,6 +1,219 @@
+require("cloud/moment.js");
+var moment = require("cloud/moment-timezone-with-data-2010-2020.min.js");
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+var find = function(objects, param, value) {
+	for (var i in objects) {
+		var object = objects[i];
+		if (object.get(param) == value) {
+			return object;
+		}
+	}
+	return null;
+}
+
+var basicUserInfo = function(user) {
+	return {
+		id: user.id,
+		name: user.get("name"),
+		picture: user.get("picture"),
+		preferences: user.get("preferences"),
+		preferenceNote: user.get("preference_note"),
+	}
+}
+
+/* Dinner
+-------------------------------------------------- */
+
+var ORDER_TIMEZONE = "US/Pacific";
+var DINNER_DEADLINE_OFFSET = {
+	hours: 16,
+	minutes: 0
+};
+
+var DinnerOrder = Parse.Object.extend("Dinner");
+
+var dinnerOrderDate = function(dateParams) {
+	var m = null;
+	if (dateParams) {
+		m = moment(new Date(dateParams.year, dateParams.month, dateParams.day));
+	} else {
+		m = moment();
+	}
+	var formattedDate = m.format("YYYY-MM-DD") 
+		+ " " + DINNER_DEADLINE_OFFSET.hours + ":" + DINNER_DEADLINE_OFFSET.minutes
+	
+	return moment.tz(formattedDate, ORDER_TIMEZONE).toDate();
+}
+
+Parse.Cloud.define("date", function(request, response) {
+	var date = dinnerOrderDate(request.params);
+	console.log(date);
+	response.success(date);
 });
+
+/* Get dinner configuration
+ * 
+ * Params: none
+ *
+ * Success returns a JSON object consisting of:
+ *	minPeople (Integer): The minimum number of people required to order dinner.
+ *	orderDeadline: a JSON object representing the deadline for ordering dinner, consisting of:
+ *		timeZone (String): The name of the timezone for which the deadline is in (eg. "US/Pacific").
+ *		offset (JSON): The time of day for the order deadline.
+ *			hours (Integer)
+ *			minutes (Integer)
+ *
+ * Possible Errors: none
+ */
+Parse.Cloud.define("dinnerGetConfigs", function(request, response) {
+	response.success({ 
+		minPeople: 4,
+		orderDeadline: {
+			timeZone: ORDER_TIMEZONE,
+			offset: DINNER_DEADLINE_OFFSET
+		}
+	});
+});
+
+/* Order dinner for the current user
+ * 
+ * Params:
+ *	specialRequest (String)
+ *	date (Date, Optional): Date for which to order dinner. If null, dinner will be ordered for today.
+ *
+ * Success: none
+ *
+ * Possible Errors:
+ * 	Invalid user
+ * 	User has already ordered dinner for the specified date.
+ */
+Parse.Cloud.define("dinnerMakeOrder", function(request, response) {
+	var orderDate = dinnerOrderDate(request.params.date);
+	console.log("Order Date: " + orderDate);
+
+	var query = new Parse.Query(DinnerOrder);
+	query.equalTo("order_date", orderDate);
+	query.equalTo("user_id", request.user.id);
+	query.first().then(function(order) {
+		if (order) {
+			response.error("User " + request.user.getEmail() + " has already ordered dinner for " + orderDate + ".");
+		} else {
+			var acl = new Parse.ACL(request.user);
+			acl.setRoleWriteAccess("Administrator", true);
+		
+			var order = new DinnerOrder();
+			order.set("user_id", request.user.id);
+			order.set("order_date", orderDate);
+			order.set("special_request", request.params.specialRequest);
+			order.setACL(acl);
+			return order.save().then(function(order) {
+				console.log("Order " + order.id + " created.");
+				response.success();
+			}, function(order, error) {
+				response.error(error);
+			});
+		}
+	},
+	function(order, error) {
+		response.error(error);
+	});
+});
+
+/* Cancel dinner order for the current user
+ *
+ * Params:
+ *	date (Date, Optional): Date for which to cancel order. If null, dinner will be cancelled for today.
+ *
+ * Success: none
+ *
+ * Possible Errors:
+ * 	Invalid user
+ * 	User has not ordered dinner for the specified date.
+ */
+Parse.Cloud.define("dinnerCancelOrder", function(request, response) {
+	var orderDate = dinnerOrderDate(request.params.date);
+	console.log("Order Date: " + orderDate);
+
+	var query = new Parse.Query(DinnerOrder);
+	query.equalTo("order_date", orderDate);
+	query.equalTo("user_id", request.user.id);
+	query.first().then(function(order) {
+		if (!order) {
+			response.error("User " + request.user.getEmail() + " has not ordered dinner for " + orderDate + ".");
+		} else {
+			return order.destroy().then(function(order) {
+				console.log("Order " + order.id + " deleted.");
+				response.success();
+			}, function(order, error) {
+				response.error(error);
+			});
+		}
+	},
+	function(order, error) {
+		response.error(error);
+	});
+});
+
+/* Returns a list of users who ordered dinner today
+ *
+ * Params:
+ *	date (JSON, Optional): Date for which to get orders for. If null, return orders for today.
+ *		day (Integer): Day of month, ranging 1-31
+ *		month (Integer): Ranging 0-11
+ *		year (Integer)
+ *
+ * Success returns: An array of JSON objects representing each person
+ * 	specialRequest (String) - A special request for today's dinner order made by the user
+ *	user (JSON):
+ *		id (String) - id of the user
+ * 		name (String) - name of the user
+ * 		picture (String) - URL of the user's avatar
+ * 		preferences (Array[Int]) - An array of integers representing the user's dietary preferences
+ *	 	preferenceNote (String) - The special dietary restriction note for the user
+ *
+ * Possible Errors: none
+ */
+Parse.Cloud.define("dinnerGetOrders", function(request, response) {
+	var orderDate = dinnerOrderDate(request.params.date);
+	console.log("Order Date: " + orderDate);
+
+	var query = new Parse.Query(DinnerOrder);
+	query.equalTo("order_date", orderDate);
+	query.find().then(function(orders) {
+		if (!orders) {
+			response.success([]);
+		} else {	
+			// TODO: Change database schema to make user a relation so this code is much simpler
+		
+			var userIds = [];
+			for (var i in orders) {
+				var order = orders[i];
+				var userId = order.get("user_id");
+				userIds.push(userId);
+			}
+		
+			var userQuery = new Parse.Query(Parse.User);
+			userQuery.ascending("name");
+			userQuery.containedIn("objectId", userIds);
+			userQuery.find().then(function(users) {
+				var results = [];
+				for (var i in users) {
+					var user = users[i];
+					var order = find(orders, "user_id", user.id);
+					results.push({
+						specialRequest: order.get("special_request"),
+						user: basicUserInfo(user)
+					});
+				}
+				response.success(results);
+			},
+			function(error) {
+				response.error(error);
+			});
+		}
+	},
+	function(error) {
+		response.error(error);
+	});
+});
+
--- api/public/scripts/dinner.js
+++ api/public/scripts/dinner.js
@@ -1,27 +1,6 @@
 $(document).ready(function() {
-    var user = Parse.User.current()
-	updateLoginInfo(user);
-
-    var now = new Date();
-    if (user) {
-        var whereClause = {
-            'user_id' : user.id,
-            'order_date': {
-                '__type': 'Date',
-                'iso': getOrderDateISO()
-            }
-        };
-        var dinnerRequest = apiRequest('/1/classes/Dinner', {where: whereClause}, 'GET');
-        if (dinnerRequest.results.length > 0){
-            $('#order-dinner').addClass('btn-danger');
-            $('#order-dinner').text('Cancel Dinner Order');
-        }
-    }
-
     $('#order-dinner').on('click', function() {
-        toggleDinner(user);
-        $(".cat-image").show();
-        updateDinnerTable();
+        toggleDinner();
     });
 
     updateDinnerTable();
@@ -36,87 +15,88 @@ function getOrderDateISO(){
     return orderDate.toISOString();
 }
 
+function updateOrderButton(ordered) {
+	$('#order-dinner').attr("ordered", ordered);
+	
+	if (ordered) {
+		$('#order-dinner').addClass('btn-danger');
+		$('#order-dinner').text('Cancel Dinner Order');
+		$('#special-notes').hide();
+	} else {
+		$('#order-dinner').removeClass('btn-danger');
+		$('#order-dinner').text('Order Dinner Tonight');
+		$('#special-notes').show();
+	}
+}
+
 function updateDinnerTable() {
-  var dinners = getOrderedDinners();
-  $tbody = $('#dinner-request-list tbody');
-  $tbody.empty();
+	Parse.Cloud.run("dinnerGetOrders").then(function(dinners) {
+		$tbody = $('#dinner-request-list tbody');
+		$tbody.empty();
 
-  for (var i = 0; i < dinners.length; i++) {
-        var request = dinners[i];
-        var currentUser = findUser(request.user_id);
-        var $row = $('<tr>');
-        var $name = $('<td>').html("<img src='" + currentUser.picture + "'> " + currentUser.name);
-        var $notes = $('<td>');
-        var pref = currentUser.preferences;
-        var $p = $('<td>');
-        if (pref) {
-            for (var j = 0; j < pref.length; j++) {
-                if (pref[j] === 0) {
-                    $p.append('<i class="icon-omnivore" alt="Omnivore" title="Omnivore" data-toggle="tooltip" data-placement="bottom"></i>&nbsp;');
-                }
-                else if (pref[j] === 1) {
-                    $p.append('<i class="icon-vegetarian" alt="Vegetarian" title="Vegetarian" data-toggle="tooltip" data-placement="bottom"></i>&nbsp;');
-                }
-                else if (pref[j] === 2) {
-                    $p.append('<i class="icon-vegan" alt="Vegan" title="Vegan" data-toggle="tooltip" data-placement="bottom"></i>&nbsp;');
-                }
+		for (var i = 0; i < dinners.length; i++) {
+			var request = dinners[i];
+			var user = request.user;
+			if (user.id == Parse.User.current().id) { // Current user has ordered
+				updateOrderButton(true);
+			}
+			
+			var $row = $('<tr>');
+			var $name = $('<td>').html("<img src='" + user.picture + "'> " + user.name);
+			var $notes = $('<td>');
+			var pref = user.preferences;
+			var $p = $('<td>');
+			if (pref) {
+				for (var j = 0; j < pref.length; j++) {
+					if (pref[j] === 0) {
+						$p.append('<i class="icon-omnivore" alt="Omnivore" title="Omnivore" data-toggle="tooltip" data-placement="bottom"></i>&nbsp;');
+					}
+					else if (pref[j] === 1) {
+						$p.append('<i class="icon-vegetarian" alt="Vegetarian" title="Vegetarian" data-toggle="tooltip" data-placement="bottom"></i>&nbsp;');
+					}
+					else if (pref[j] === 2) {
+						$p.append('<i class="icon-vegan" alt="Vegan" title="Vegan" data-toggle="tooltip" data-placement="bottom"></i>&nbsp;');
+					}
 
-                if (pref[j] === 3) {
-                    $p.append('<i class="icon-gluten" alt="No Gluten" title="No Gluten" data-toggle="tooltip" data-placement="bottom"></i>&nbsp;');
-                }
-            }
-        }
-        if (request.special_request) {
-            $notes.append(request.special_request);
-        }
-        $row.append($name).append($p).append($notes);
-        $tbody.append($row);
-        $('[data-toggle="tooltip"]').tooltip();
-    }
+					if (pref[j] === 3) {
+						$p.append('<i class="icon-gluten" alt="No Gluten" title="No Gluten" data-toggle="tooltip" data-placement="bottom"></i>&nbsp;');
+					}
+				}
+			}
+			if (request.specialRequest) {
+				$notes.append(request.specialRequest);
+			}
+			$row.append($name).append($p).append($notes);
+			$tbody.append($row);
+			$('[data-toggle="tooltip"]').tooltip();
+		}
+	});
 }
 
-function getOrderedDinners() {
-    var whereClause = {
-        'order_date': {
-            '__type': 'Date',
-            'iso': getOrderDateISO()
-        }
-    };
-    var dinnerRequest = apiRequest('/1/classes/Dinner', {where: whereClause, order: 'createdAt'}, 'GET');
-    return dinnerRequest.results;
-}
-
-function toggleDinner(user) {
-    var whereClause = {
-        'user_id' : user.id,
-        'order_date': {
-            '__type': 'Date',
-            'iso': getOrderDateISO()
-        }
-    };
-    var dinnerRequest = apiRequest('/1/classes/Dinner', {where: whereClause}, 'GET');
-    if (dinnerRequest.results.length > 0) {
-        apiRequest('/1/classes/Dinner/' + dinnerRequest.results[0].objectId , {}, 'DELETE');
-        $('#order-dinner').removeClass('btn-danger');
-        $('#order-dinner').text('Order Dinner Tonight');
-    }
-    else {
+function toggleDinner() {
+	var ordered = $('#order-dinner').attr("ordered");
+	if (!ordered) {
+        var params = null;
         var specialRequest = $('#special-notes').val();
-        $('#special-notes').val('');
-        var data = {
-            'picture': user.getPicture(),
-            'name': user.getName(),
-            'user_id' : user.id,
-            'special_request' : specialRequest,
-            'order_date': {
-                '__type': 'Date',
-                'iso': getOrderDateISO()
-            }
-        };
-        apiRequest('/1/classes/Dinner', data ,'POST');
-        $('#order-dinner').addClass('btn-danger');
-        $('#order-dinner').text('Cancel Dinner Order');
-    }
+        if (specialRequest != '') {
+        	params = {
+        		specialRequest: specialRequest
+        	}
+        }
+        
+		Parse.Cloud.run("dinnerMakeOrder", params).then(function(order) {
+			$('#special-notes').val('');
+			updateOrderButton(true);
+			$(".cat-image").show();
+			updateDinnerTable();
+		});
+	} else {
+		Parse.Cloud.run("dinnerCancelOrder").then(function(order) {
+			updateOrderButton(false);
+			$(".cat-image").hide();
+			updateDinnerTable();
+		});
+	}
 }
 
 function countdown() {
