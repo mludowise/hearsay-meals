9da51d1e64886409a20d0d591967385405f1481a
mludowise <mludowise@gmail.com>
2015-01-31 14:58:56 -0800

    View beer requests

--- api/cloud/main.js
+++ api/cloud/main.js
@@ -1,3 +1,4 @@
+// moment.js required for timezone manipulation
 require("cloud/moment.min.js");
 var moment = require("cloud/moment-timezone-with-data-2010-2020.min.js");
 
@@ -11,27 +12,35 @@ var find = function(objects, param, value) {
 	return null;
 }
 
-var basicUserInfo = function(user) {
-	return {
+var userInfo = function(user, includePrefs) {
+	var user = {
 		id: user.id,
 		name: user.get("name"),
 		picture: user.get("picture"),
-		preferences: user.get("preferences"),
-		preferenceNote: user.get("preference_note"),
+	};
+	if (includePrefs) {
+		user.preferences = user.get("preferences");
+		user.preferenceNote = user.get("preference_note");
 	}
+	return user;
 }
 
 /* Dinner
 -------------------------------------------------- */
 
+// Constants we'll be using to calculate when to order dinner
 var DINNER_ORDER_TIMEZONE = "US/Pacific";
 var DINNER_ORDER_DEADLINE = {
 	hours: 16,
 	minutes: 0
 };
 
+// Convenience class
 var DinnerOrder = Parse.Object.extend("Dinner");
 
+// Utility to generate a Date object for the dinner order given a date.
+// Will set the time of the date to the dinner order deadline.
+// If dateParams are provided, it will set the day/month/year to the appropriate date.
 var dinnerOrderDate = function(dateParams) {
 	var m = null;
 	if (dateParams) {
@@ -44,12 +53,6 @@ var dinnerOrderDate = function(dateParams) {
 	
 	return moment.tz(formattedDate, DINNER_ORDER_TIMEZONE).toDate();
 }
-
-Parse.Cloud.define("date", function(request, response) {
-	var date = dinnerOrderDate(request.params);
-	console.log(date);
-	response.success(date);
-});
 
 /* Get dinner configuration
  * 
@@ -88,6 +91,11 @@ Parse.Cloud.define("dinnerGetConfigs", function(request, response) {
  * 	User has already ordered dinner for the specified date.
  */
 Parse.Cloud.define("dinnerMakeOrder", function(request, response) {
+	if (!request.user) {
+		response.error("A dinner order can only be made when a user is logged in.");
+		return;
+	}
+	
 	var orderDate = dinnerOrderDate(request.params.date);
 	console.log("Order Date: " + orderDate);
 
@@ -131,6 +139,11 @@ Parse.Cloud.define("dinnerMakeOrder", function(request, response) {
  * 	User has not ordered dinner for the specified date.
  */
 Parse.Cloud.define("dinnerCancelOrder", function(request, response) {
+	if (!request.user) {
+		response.error("A dinner order can only be cancelled when a user is logged in.");
+		return;
+	}
+	
 	var orderDate = dinnerOrderDate(request.params.date);
 	console.log("Order Date: " + orderDate);
 
@@ -202,7 +215,7 @@ Parse.Cloud.define("dinnerGetOrders", function(request, response) {
 					var order = find(orders, "user_id", user.id);
 					results.push({
 						specialRequest: order.get("special_request"),
-						user: basicUserInfo(user)
+						user: userInfo(user, true)
 					});
 				}
 				response.success(results);
@@ -217,3 +230,196 @@ Parse.Cloud.define("dinnerGetOrders", function(request, response) {
 	});
 });
 
+
+/* Beer
+-------------------------------------------------- */
+
+// Convenience classes
+var BeerReequest = Parse.Object.extend("BeerRequest");
+var Keg = Parse.Object.extend("Keg");
+
+/* Returns the beer that's on tap
+ *
+ * Params: none
+ *
+ * Success: Returns JSON object of the most recent beer on tap or null if there isn't one.
+ *	name (String): Name of the beer on tap
+ *	filled (Date): Date that the keg was filled
+ *	kickedReport (JSON Array): A list of users who reported the keg was kicked.
+ *		id (String) - id of the user
+ * 		name (String) - name of the user
+ * 		picture (String) - URL of the user's avatar
+ *
+ * Possible Errors: none
+ */
+Parse.Cloud.define("beerOnTap", function(request, response) {
+	response.success();
+});
+
+/* Marks that the current user reported the keg was kicked.
+ *
+ * Params: none
+ *
+ * Success: Returns a JSON array of users who reported the keg was kicked.
+ *		id (String) - id of the user
+ * 		name (String) - name of the user
+ * 		picture (String) - URL of the user's avatar
+ *
+ * Possible Errors:
+ *	There is no user currently logged in.
+ */
+Parse.Cloud.define("beerReportKicked", function(request, response) {
+	response.success();
+});
+
+/* Removes this user's report that the keg is kicked.
+ *
+ * Params: none
+ *
+ * Success: Returns a JSON array of users who reported the keg was kicked.
+ *		id (String) - id of the user
+ * 		name (String) - name of the user
+ * 		picture (String) - URL of the user's avatar
+ *
+ * Possible Errors:
+ *	There is no user currently logged in.
+ */
+Parse.Cloud.define("beerUnreportKicked", function(request, response) {
+	response.success();
+});
+
+/* Marks the keg as filled with a specific beer.
+ *
+ * Params:
+ *	name (String): Name of the beer
+ *
+ * Success: none
+ *
+ * Possible Errors:
+ *	The current user does not have permission to fill the keg.
+ */
+Parse.Cloud.define("beerFillKeg", function(request, response) {
+	response.success();
+});
+
+/* Marks the keg as filled by a specific beer requested.
+ * The beer request will be removed.
+ *
+ * Params:
+ *	requestId (String): The id of the beer request 
+ *
+ * Success: none
+ *
+ * Possible Errors:
+ *	There is no user currently logged in.
+ *	The current user does not have permission to fill the keg.
+ *	There is no request with the specified id.
+ */
+Parse.Cloud.define("beerFillKegFromRequest", function(request, response) {
+	response.success();
+});
+
+var possiblySucceed = function(response, results, result, numItems) {
+	results.push(result);
+	if (results.length == numItems) {
+		response.success(results);
+	}
+}
+
+
+/* Get beer requests.
+ *
+ * Params: none
+ *
+ * Success: List of JSON objects containing
+ *	id (String): ID of the beer request
+ *	name (String): Name of the beer
+ *	votes (JSON Array): A list of users who reported the keg was kicked.
+ *		id (String) - id of the user
+ * 		name (String) - name of the user
+ * 		picture (String) - URL of the user's avatar
+ *
+ * Possible Errors: none
+ * 
+ */
+Parse.Cloud.define("beerGetRequests", function(request, response) {
+	var query = new Parse.Query(BeerReequest);
+	query.ascending("name");
+	query.find().then(function(beerRequests) {
+		// First get a combined list of all the users who voted
+		var userIds = [];
+		for (var r in beerRequests) {
+			var beerRequest = beerRequests[r];
+			var votes = beerRequest.get("votes");
+			for (var u in votes) {
+				userIds.push(votes[u])
+			}
+		}
+		
+		var userMap = {};
+		var userQuery = new Parse.Query(Parse.User);
+		userQuery.ascending("name");
+		userQuery.containedIn("objectId", userIds);
+		userQuery.find().then(function(users) {
+			// Find all the users who voted
+			for (var u in users) {
+				var user = users[u];
+				userMap[user.id] = userInfo(user, false);
+			}
+			
+			// Add them to the results
+			var results = [];
+			for (var r in beerRequests) {
+				var beerRequest = beerRequests[r];
+				var votes = beerRequest.get("votes");
+				var usersVoted = [];
+				for (var u in votes) {
+					var userId = votes[u];
+					usersVoted.push(userMap[userId]);
+				}
+				results.push({
+					id: beerRequest.id,
+					name: beerRequest.get("name"),
+					votes: usersVoted
+				});
+			}
+			response.success(results);
+		}, function(users, error) {
+			response.error(error);
+		});
+	}, function(beerRequests, error) {
+		response.error(error);
+	});
+});
+
+/* Request a beer.
+ *
+ * Params:
+ *	name (String): Name of the beer to request.
+ *
+ * Success: none
+ *
+ * Possible Errors:
+ *	There is no user currently logged in.
+ *	The specified beer has already been requested.
+ * 
+ */
+Parse.Cloud.define("beerAddRequest", function(request, response) {
+	response.success();
+});
+
+/* Clear a beer request.
+ *
+ * Params:
+ *	id (String): The id of the request to remove
+ *
+ * Success: none
+ *
+ * Possible Errors:
+ *	There is no user currently logged in.
+ *	The current user does not have permission to delete a request.
+ *	There is no request with the specified id.
+ */
+Parse.Cloud.define("beerRemoveRequest", function(request, response) {
+	response.success();
+});
